0477e218fbe449ee55065ebe4046f25896c27c28
stephanie953 <stephanie_scholtes@hotmail.com>
2018-03-06 14:55:36 +0100

    06-03

--- apitutorial.js
+++ apitutorial.js
@@ -1,60 +1,40 @@
 $(document).ready(function() {
     $("#submitButton").on("click", function() {
         let artist = $("#artists").val();
+        let noOfSongs = $("#noOfSongs").val();
         let songs = [];
         let albums = [];
         let songPrices = [];
+        let soundPreviews = [];
 
         $.ajax({
-            url: "https://itunes.apple.com/search?entity=song&limit=5&term=" + artist
+            url: "https://itunes.apple.com/search?entity=song&country=us&limit=" + noOfSongs + "&term=" + artist
         }).done(response => {
             let res = JSON.parse(response);
-
+          
             let resultsarray = res.results;
-
             for (let i = 0; i < resultsarray.length; i++) {
                 songs.push(res.results[i].trackName);
                 albums.push(res.results[i].collectionName);
                 songPrices.push(res.results[i].trackPrice);
+                soundPreviews.push(res.results[i].previewUrl);
+            }
+      
+            let tableHeader = "<thead><tr><th scope='col'>#</th><th scope='col'>Song</th><th scope='col'>Album</th><th scope='col'>Price</th><th scope='col'>Preview</th></tr></thead>";
+
+            let tableRow = [];
+
+            for (let i = 0; i < resultsarray.length; i++) {
+                tableRow.push("<tr><th scope='row'>" + (i+1) + "</th><td>" + songs[i] + "</td><td>" + albums[i] + "</td><td>$"+ songPrices[i] + "</td><td>" + 
+                "<audio controls><source src=" + soundPreviews[i] + "/></audio></td></tr>");
             }
 
-            console.log(songs);
-            console.log(albums);
-            console.log(songPrices);
+            let table = tableHeader + "<tbody>" + tableRow + "</tbody>";
+            $("table").empty();
+            $("table").append(table);
         });
-
     });
+
 });
 
 
-
-
-
-            // let table = "<canvas id='myChart'></canvas>";
-            // $("body").append(table);
-
-            // if (chart) {
-            //     chart.destroy();
-            // }
-
-        //     var ctx = document.getElementById("myChart").getContext("2d");
-
-        //     var chart = new Chart(ctx, {
-        //         // the type of chart
-        //         type: 'line',
-
-        //         //the data for the chart
-        //         data: {
-        //             labels: labels,
-        //             datasets: [{
-        //                 label: 'Historical BPI data',
-        //                 data: data,
-        //                 backgroundColor: "rgb(255,99,132)",
-        //                 borderColor: "rgb(255, 99, 132)"
-        //             }]
-        //         },
-
-        //         //configuration options go here
-        //         options: {}
-
-        //     });
